d8121f920e764ae4b7ed21fcf10babcfb36cf9a6
streamamz <polish.stream@gmail.com>
2025-03-30 04:11:08 +0900

    upload constant.py

--- MDUS/__init__.py
+++ MDUS/__init__.py
@@ -1,6 +1,13 @@
 # version 0.1.0
 __version__ = "0.1.0"
 
+# necessary imports
+import numpy as np
+import pandas as pd
+
+# import constant
+from .Constant import constant
+
 # import Classes
 from .Class.MagDataClass import MagData
 from .Class.ScanDataClass import ScanData
