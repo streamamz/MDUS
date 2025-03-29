ccaa187980cae5082827af0794ae2bf7cbd6bbb5
streamamz <polish.stream@gmail.com>
2025-03-30 03:38:15 +0900

    upload setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# setup
+setup(
+    name="MDUS",
+    version = "0.1.0",
+    author = "Takuro OGAWA",
+    author_email = "polish.stream@gmail.com",
+    description = "MDUS: MESSENGER's Data Using System",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url = "https://github.com/streamamz/MDUS",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    include_package_data=True
+)
