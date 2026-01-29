ac6fe7af507a0ee41bb21785db0a1aa072b49f01
streamamz <polish.stream@gmail.com>
2026-01-29 18:47:04 +0900

    updater version to 0.1.8

--- MDUS/__init__.py
+++ MDUS/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 # Spice setup
 from MDUS.Spice.SpiceSetup import *
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ print("Setting up MDUS package...")
 # setup
 setup(
     name="MDUS",
-    version = "0.1.7",
+    version = "0.1.8",
     author = "Takuro OGAWA",
     author_email = "polish.stream@gmail.com",
     description = "MDUS: MESSENGER's Data Using System",
